cb76f907afc83e7a179d7201e0de77ea2824ef8a
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2023-07-25 10:38:07 -0300

    chore(auth): refactored DbContext

--- src/api/TechNews.Auth.Api/Data/AuthDbContext.cs
+++ src/api/TechNews.Auth.Api/Data/AuthDbContext.cs
@@ -14,23 +14,24 @@ public class AuthDbContext : IdentityDbContext<User, Role, Guid>
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<User>(b => b.ToTable("Users"));
-        modelBuilder.Entity<Role>(b => b.ToTable("Roles"));
+        modelBuilder.Entity<User>(b => b.ToTable("Users").HasQueryFilter(p => !p.IsDeleted));
+        modelBuilder.Entity<Role>(b => b.ToTable("Roles").HasQueryFilter(p => !p.IsDeleted));
         modelBuilder.Entity<IdentityUserClaim<Guid>>(b => b.ToTable("UserClaims"));
         modelBuilder.Entity<IdentityUserLogin<Guid>>(b => b.ToTable("UserLogins"));
         modelBuilder.Entity<IdentityUserToken<Guid>>(b => b.ToTable("UserTokens"));
         modelBuilder.Entity<IdentityRoleClaim<Guid>>(b => b.ToTable("RoleClaims"));
         modelBuilder.Entity<IdentityUserRole<Guid>>(b => b.ToTable("UserRoles"));
 
-        modelBuilder.Entity<User>().HasQueryFilter(p => !p.IsDeleted);
-        modelBuilder.Entity<Role>().HasQueryFilter(p => !p.IsDeleted);
+        DefineDeleteStrategy(modelBuilder);
+        DefineColumnsWithoutMaxLength(modelBuilder);
+    }
 
+    private static void DefineDeleteStrategy(ModelBuilder modelBuilder)
+    {
         foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
         {
             relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
         }
-
-        DefineColumnsWithoutMaxLength(modelBuilder);
     }
 
     private static void DefineColumnsWithoutMaxLength(ModelBuilder modelBuilder)
