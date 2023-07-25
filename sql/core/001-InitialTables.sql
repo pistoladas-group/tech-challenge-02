19229eadc8cfd87c8fa76f7c22dc6350adbe4f0e
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2023-07-25 10:44:09 -0300

    chore(core): first migration and scripts

---
+++ sql/core/001-InitialTables.sql
@@ -0,0 +1,58 @@
+﻿IF OBJECT_ID(N'[_AppliedMigrations]') IS NULL
+BEGIN
+    CREATE TABLE [_AppliedMigrations] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK__AppliedMigrations] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [_AppliedMigrations] WHERE [MigrationId] = N'20230725131555_FirstMigration')
+BEGIN
+    CREATE TABLE [Authors] (
+        [Id] uniqueidentifier NOT NULL,
+        [Name] VARCHAR(500) NOT NULL,
+        [Email] VARCHAR(500) NOT NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        [IsDeleted] bit NOT NULL,
+        CONSTRAINT [PK_Authors] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [_AppliedMigrations] WHERE [MigrationId] = N'20230725131555_FirstMigration')
+BEGIN
+    CREATE TABLE [News] (
+        [Id] uniqueidentifier NOT NULL,
+        [Title] VARCHAR(500) NOT NULL,
+        [Description] VARCHAR(500) NOT NULL,
+        [PublishDate] datetime2 NOT NULL,
+        [AuthorId] uniqueidentifier NOT NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        [IsDeleted] bit NOT NULL,
+        CONSTRAINT [PK_News] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_News_Authors_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Authors] ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [_AppliedMigrations] WHERE [MigrationId] = N'20230725131555_FirstMigration')
+BEGIN
+    CREATE INDEX [IX_News_AuthorId] ON [News] ([AuthorId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [_AppliedMigrations] WHERE [MigrationId] = N'20230725131555_FirstMigration')
+BEGIN
+    INSERT INTO [_AppliedMigrations] ([MigrationId], [ProductVersion])
+    VALUES (N'20230725131555_FirstMigration', N'7.0.3');
+END;
+GO
+
+COMMIT;
+GO
+
---
+++ src/api/TechNews.Core.Api/Migrations/20230725131555_FirstMigration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechNews.Core.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(500)", nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(500)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "VARCHAR(500)", nullable: false),
+                    Description = table.Column<string>(type: "VARCHAR(500)", nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_News_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_AuthorId",
+                table: "News",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
